2af6acab83d9d70af98271f860c6cd3ab0769f07
safayasmin <safayasmin567@gmail.com>
2026-05-01 10:04:37 +0530

    Time: 0 ms (100%), Space: 54.4 MB (18.1%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    let count=0;
+   for (let i=1;i<=n;i++){
+    if(n%i===0){
+        count++;
+    }
+   } 
+   return count===3
+};
+console.log(isThree(2))
