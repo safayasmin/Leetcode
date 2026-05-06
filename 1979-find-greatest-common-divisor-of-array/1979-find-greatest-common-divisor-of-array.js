afb3f06bb7ea0620e617863a646f85f53652b7fe
safayasmin <safayasmin567@gmail.com>
2026-05-06 10:01:22 +0530

    Time: 1 ms (40.68%), Space: 54 MB (67.68%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var findGCD = function(nums) {
-   var min = Math.min(...nums)
-   var max = Math.max(...nums)
-
-   var dev = 1
-
-   for(var i = 1; i <= min; i++){
-      if(min % i == 0 && max % i == 0){
-         dev = i
-      }
-   }
-
-   return dev
+    var min=Math.min(...nums)
+    var max=Math.max(...nums)
+    for(var i=1;i<=min;i++){
+        if(min%i==0&&max%i==0){
+            var div=i
+        }
+    }
+    return div;
+    
 };
-
-console.log(findGCD([3,3]));
+console.log(findGCD([3,3]))
