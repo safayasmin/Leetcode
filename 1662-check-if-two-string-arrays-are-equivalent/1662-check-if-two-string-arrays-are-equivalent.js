ba1541b2eda9b40db3dd6f5c50ee23383aa00ead
safayasmin <safayasmin567@gmail.com>
2026-05-07 19:34:53 +0530

    Time: 0 ms (100%), Space: 54.8 MB (13.85%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+
+var arrayStringsAreEqual = function(word1, word2) {
+   if(word1.join("")===word2.join("")){
+       return true;
+   }else{
+       return false;
+   }
+};
+console.log(arrayStringsAreEqual(["abc", "d", "defg"],["abcddefg"]))
