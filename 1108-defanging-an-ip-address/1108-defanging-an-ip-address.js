dab4c1ca45c8bf887fd599c098d1edb60484475c
safayasmin <safayasmin567@gmail.com>
2026-05-25 18:03:36 +0530

    Time: 33 ms (95.37%), Space: 53.3 MB (76.61%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    var arr=[]
+    for(i=0;i<address.length;i++){
+        if(address[i]==="."){
+            arr.push("[.]")
+        }else{
+            arr.push(address[i])
+        }
+    }
+    return arr.join("").toString()
+};
